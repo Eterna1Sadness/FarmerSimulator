f0b815c960fd6e0da5f140f7062a91facbeba81c
Ilya <ilyachernetskii@gmail.com>
2021-05-13 18:29:17 +0300

    init project commit

---
+++ Assets/SuperTiled2Unity/Import/House.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="House" tilewidth="16" tileheight="16" tilecount="459" columns="17">
+ <image source="houses.png" width="272" height="432"/>
+</tileset>
