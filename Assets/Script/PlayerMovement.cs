30f6ece0db2b163acbd3e29eddf2fdb549561db3
Ilya <ilyachernetskii@gmail.com>
2021-05-22 23:54:50 +0300

    Добавил анимацию кирки и топора. Написал скрипт рубки дерева и переписал скрипт ходьбы.

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,12 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    walk,
+    dig,
+    axe,
+    pick,
+    interact
+}
+
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    public PlayerState currentState;
     public float speed;
     private Rigidbody2D myRigidBody;
-    private Vector3 change;
+    public Vector2 change;
+    public Vector2 lastMotionVector;
     private Animator animator;
+    private bool moving;
 
     // Start is called before the first frame update
     void Start()
@@ -19,29 +32,103 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        change = Vector3.zero;
-        change.x = Input.GetAxisRaw("Horizontal");
-        change.y = Input.GetAxisRaw("Vertical");
-        UpdateAnimationAndMove();
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        change = new Vector2(horizontal, vertical);
+
+        animator.SetFloat("lastHorizontal", horizontal);
+        animator.SetFloat("lastVertical", vertical);
+
+        moving = horizontal != 0 || vertical != 0;
+        animator.SetBool("moving", moving);
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            lastMotionVector = new Vector2(horizontal, vertical).normalized;
+            animator.SetFloat("lastHorizontal", horizontal);
+            animator.SetFloat("lastVertical", vertical);
+        }
+
+        //change = Vector2.zero;
+        //change.x = Input.GetAxisRaw("Horizontal");
+        //change.y = Input.GetAxisRaw("Vertical");
+        //if(Input.GetButtonDown("dig") && currentState != PlayerState.dig)
+        //{
+        //    StartCoroutine(DigCo());
+        //}
+        //else if (Input.GetButtonDown("axe") && currentState != PlayerState.axe)
+        //{
+        //    StartCoroutine(AxeCo());
+        //}
+        //else if (Input.GetButtonDown("pick") && currentState != PlayerState.pick)
+        //{
+        //    StartCoroutine(PickCo());
+        //}
+        //else if(currentState == PlayerState.walk)
+        //{
+        //    UpdateAnimationAndMove();
+        //}
+        //UpdateAnimationAndMove();
     }
 
-    void UpdateAnimationAndMove()
+    private IEnumerator DigCo()
     {
-        if (change != Vector3.zero)
-        {
-            MoveCharacter();
-            animator.SetFloat("moveX", change.x);
-            animator.SetFloat("moveY", change.y);
-            animator.SetBool("moving", true);
-        }
-        else
-        {
-            animator.SetBool("moving", false);
-        }
+        animator.SetBool("digging", true);
+        currentState = PlayerState.dig;
+        yield return null;
+        animator.SetBool("digging", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
     }
 
-    void MoveCharacter()
+    private IEnumerator AxeCo()
     {
-        myRigidBody.MovePosition(transform.position + change * speed * Time.deltaTime);
+        animator.SetBool("axing", true);
+        currentState = PlayerState.axe;
+        yield return null;
+        animator.SetBool("axing", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
+    }
+
+    private IEnumerator PickCo()
+    {
+        animator.SetBool("picking", true);
+        currentState = PlayerState.pick;
+        yield return null;
+        animator.SetBool("picking", false);
+        yield return new WaitForSeconds(.3f);
+        currentState = PlayerState.walk;
+    }
+
+    //void UpdateAnimationAndMove()
+    //{
+    //    if (change != Vector2.zero)
+    //    {
+    //        MoveCharacter();
+    //        animator.SetFloat("moveX", change.x);
+    //        animator.SetFloat("moveY", change.y);
+    //        animator.SetBool("moving", true);
+    //    }
+    //    else
+    //    {
+    //        animator.SetBool("moving", false);
+    //    }
+    //}
+
+    //void MoveCharacter()
+    //{
+    //    myRigidBody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    //}
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        myRigidBody.velocity = change * speed;
     }
 }
